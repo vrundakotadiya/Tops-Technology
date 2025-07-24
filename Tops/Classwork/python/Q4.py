420608e911ebcace3465cfbc42a3e80b7a2a62bb
vrundakotadiya <vrundakotadiya26@gmail.com>
2025-07-24 17:33:27 +0530

    Add files via upload
    excel ass 1 and soft skill 1

---
+++ Tops/Classwork/python/Q4.py
@@ -0,0 +1,8 @@
+number= int(input("enter a number:-"))
+
+if number>0:
+    print("number is positive",number)
+elif number<0:
+    print("number is negative",number)
+else:
+    print("number is zero")
